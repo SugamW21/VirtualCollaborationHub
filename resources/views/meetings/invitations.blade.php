4c5b76054de433bc2e7d99a82ddeecf0536ee378
SugamW21 <wostisugam@gmail.com>
2025-03-28 11:24:09 +0545

    feature enhancement

--- resources/views/meetings/invitations.blade.php
+++ resources/views/meetings/invitations.blade.php
@@ -9,6 +9,7 @@
                 <form action="{{ route('meetings.respond', $invitation->id) }}" method="POST">
                     @csrf
                     <button type="submit" name="status" value="accepted">Accept</button>
+                    <button type="submit" name="status" value="accepted">Accept</button>
                     <button type="submit" name="status" value="rejected">Reject</button>
                 </form>
             @elseif ($invitation->status == 'accepted')
